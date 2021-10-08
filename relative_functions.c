133923cd6957eab31be0c50a8289c9a17bfe8d10
ulisesjiu <84552781+ulisesjiu@users.noreply.github.com>
2021-10-08 13:20:54 -0700

    Add files via upload

---
+++ relative_functions.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <float.h>
+#include <string.h>
+#include <stdbool.h>
+#include <math.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <gsl/gsl_sf_gamma.h>
+#include <gsl/gsl_rng.h>
+#include <gsl/gsl_randist.h>
+#include <gsl/gsl_fit.h>
+#include <err.h>
+#include "pcg_basic.h"
+
+#define LSB(i) ((i) & -(i)) //isolates least significant single bit
+#define PI 3.141592654
+
