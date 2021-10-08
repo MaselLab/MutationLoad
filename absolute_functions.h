aff0aafeabdc3297f3bd2c93ed789ede6b72582c
ulisesjiu <84552781+ulisesjiu@users.noreply.github.com>
2021-10-08 13:24:28 -0700

    Add files via upload

---
+++ absolute_functions.h
@@ -0,0 +1,20 @@
+#ifndef ABSOLUTE_FITNESS_H_INCLUDED
+#define ABSOLUTE_FITNESS_H_INCLUDED 1
+
+#include <stdio.h>
+#include <float.h>
+#include <string.h>
+#include <stdbool.h>
+#include <math.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <gsl/gsl_sf_gamma.h>
+#include <gsl/gsl_rng.h>
+#include <gsl/gsl_randist.h>
+#include <gsl/gsl_fit.h>
+#include <err.h>
+#include "pcg_basic.h"
+
+
+#endif // ABSOLUTE_FITNESS_H_INCLUDED
