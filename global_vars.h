80f3c0b60e2a093dd19fc8d06df9c4a60996bc37
ulisesjiu <84552781+ulisesjiu@users.noreply.github.com>
2021-10-12 19:40:27 -0700

    Add files via upload

---
+++ global_vars.h
@@ -0,0 +1,22 @@
+#ifndef GLOBAL_VARS_H_INCLUDED
+#define GLOBAL_VARS_H_INCLUDED 1
+
+#define VERBOSE 0
+#define VERYVERBOSE 1
+#define MISCELLANEOUS 1
+#define INDIVIDUALWIDATA 1
+#define OVERLAPPINGGENERATIONS 1 //probably should be an input argument at some point.
+#define LSB(i) ((i) & -(i)) //isolates least significant single bit for fenwick tree
+#define PI 3.141592654
+
+//bug checking files
+FILE	*verbosefilepointer;
+FILE	*veryverbosefilepointer;
+FILE	*miscfilepointer;
+//simulation data files
+FILE	*rawdatafilepointer;
+FILE    *summarydatafilepointer;
+FILE    *finaldatafilepointer;
+
+#endif // GLOBAL_VARS_H_INCLUDED
+
